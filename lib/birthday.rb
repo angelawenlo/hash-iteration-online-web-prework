f4f923c81e3015dce0ea3d6c7eab177b7061dbc8
Angela Lo <angelawenlo@gmail.com>
2019-05-17 16:59:21 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -8,4 +8,5 @@ def birthday(birthday_kids)
 birthday_message = ""
 birthday_kids.each do |name, age|
   birthday_message = "Happy Birthday #{name}! You are now #{age} years old!"
+  birthday_message
 end
